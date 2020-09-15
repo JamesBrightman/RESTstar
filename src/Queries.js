720bcfd9f8485e5b2aacac9bc64d05c2fc4b0e9c
James <jamesmbrightman@gmail.com>
2020-09-16 00:15:54 +0100

    firebase init

--- src/Queries.js
+++ src/Queries.js
@@ -1,9 +1,9 @@
 export const fetchPlanets = async(key, page) => {
-    const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+    const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
     return res.json(); //gets the data - returns a promise
 };
 
 export const fetchPeople = async() => {
-    const res = await fetch("http://swapi.dev/api/people");
+    const res = await fetch("https://swapi.dev/api/people");
     return res.json(); //gets the data - returns a promise
 };
