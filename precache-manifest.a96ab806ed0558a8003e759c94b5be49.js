372a9719aef9d40112da1423c2e58f3ee2acb691
James <jamesmbrightman@gmail.com>
2020-09-16 10:57:25 +0100

    Updates

---
+++ precache-manifest.a96ab806ed0558a8003e759c94b5be49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31db4a605bf67b0e6d5716726144a28b",
+    "url": "/RESTstar/index.html"
+  },
+  {
+    "revision": "186bf8694802f74ae860",
+    "url": "/RESTstar/static/css/main.53ac3328.chunk.css"
+  },
+  {
+    "revision": "4a1bb142aa5ec4b86b95",
+    "url": "/RESTstar/static/js/2.f3f55c0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTstar/static/js/2.f3f55c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186bf8694802f74ae860",
+    "url": "/RESTstar/static/js/main.9e57212b.chunk.js"
+  },
+  {
+    "revision": "14f1084c65dfaf96a906",
+    "url": "/RESTstar/static/js/runtime-main.420557b1.js"
+  }
+]);
